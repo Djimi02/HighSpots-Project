1e930ef8f7e392709fccfbaa348c2e59bd8c6002
Djimi02 <mitko.manolev1@abv.bg>
2023-04-28 23:00:17 +0200

    initial user model added

---
+++ project/app/src/main/java/com/example/highspots/models/User.java
@@ -0,0 +1,38 @@
+package com.example.highspots.models;
+
+public class User {
+
+    private String dbID;
+    private String nickName;
+    private String email;
+
+    public User(String dbID, String nickName, String email) {
+        this.dbID = dbID;
+        this.nickName = nickName;
+        this.email = email;
+    }
+
+    public String getDbID() {
+        return dbID;
+    }
+
+    public void setDbID(String dbID) {
+        this.dbID = dbID;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
