5f49bbce6fef290be860d70b99bbb57e082263e8
Djimi02 <mitko.manolev1@abv.bg>
2023-04-29 21:38:28 +0200

    settings page added
    simple log out button was added

--- project/app/src/main/java/com/example/highspots/HomePageActivity.java
+++ project/app/src/main/java/com/example/highspots/HomePageActivity.java
@@ -2,14 +2,35 @@ package com.example.highspots;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    /* Views */
+    private ImageButton goToSettingsBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         this.getSupportActionBar().hide();
+
+        initViews();
+    }
+
+    private void initViews() {
+        this.goToSettingsBTN = findViewById(R.id.HomePageSettingsBTN);
+
+        goToSettingsBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ project/app/src/main/java/com/example/highspots/SettingsActivity.java
@@ -0,0 +1,49 @@
+package com.example.highspots;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    /* Views */
+    private Button logOutBTN;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        this.getSupportActionBar().hide();
+
+        initViews();
+    }
+
+    private void initViews() {
+        this.logOutBTN = findViewById(R.id.SettingsPageLogOutBTN);
+
+        logOutBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLoginPage();
+                Toast.makeText(SettingsActivity.this, "LogOut was successful!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /** Redirects to login page and clears the back stack. */
+    private void goToLoginPage() {
+        FirebaseAuth.getInstance().signOut();
+
+        Intent intent = new Intent(SettingsActivity.this, LogInActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+}
