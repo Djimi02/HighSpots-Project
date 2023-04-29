41d3d1cbb0428e3c6a14b91d58479ca0b31616eb
Djimi02 <mitko.manolev1@abv.bg>
2023-04-29 20:37:32 +0200

    user login and registration implemented

--- project/app/src/main/java/com/example/highspots/LogInActivity.java
+++ project/app/src/main/java/com/example/highspots/LogInActivity.java
@@ -1,15 +1,130 @@
 package com.example.highspots;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class LogInActivity extends AppCompatActivity {
+
+    /* Views */
+    private EditText emailET;
+    private EditText passwordET;
+    private Button logInBTN;
+    private TextView goToRegisterBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         this.getSupportActionBar().hide();
+
+        initViews();
+    }
+
+    private void initViews() {
+        this.emailET = findViewById(R.id.LogInPageEmailET);
+        this.passwordET = findViewById(R.id.LogInPagePasswordET);
+        this.logInBTN = findViewById(R.id.LogInPageLogInBTN);
+        this.goToRegisterBTN = findViewById(R.id.LogInPageRegisterBTN);
+
+        logInBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logIn();
+                hideSoftKeyboard(LogInActivity.this);
+            }
+        });
+
+        goToRegisterBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LogInActivity.this, RegistrationActivity.class));
+            }
+        });
+    }
+
+    private boolean isDataValid() {
+        boolean output = true;
+
+        String email = emailET.getText().toString().trim();
+        String password = passwordET.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            emailET.setError("Input your email!");
+            output = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailET.setError("Input correct email!");
+            output = false;
+        }
+
+        if (password.isEmpty()) {
+            passwordET.setError("Input your password!");
+            output = false;
+        } else if (password.length() < 6) {
+            passwordET.setError("Password should be at least 6 characters long!");
+            output = false;
+        }
+
+        return output;
+    }
+
+    private void logIn() {
+        if (!isDataValid()) {
+            return;
+        }
+
+        String email = emailET.getText().toString().trim();
+        String password = passwordET.getText().toString().trim();
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LogInActivity.this, "LogIn was successful!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LogInActivity.this, HomePageActivity.class));
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof FirebaseAuthInvalidUserException) {
+                    Toast.makeText(LogInActivity.this, "There is no user with those credentials!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LogInActivity.this, "Registration was not successful! Please try again later!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void hideSoftKeyboard(View view){
+        InputMethodManager imm =(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    private void hideSoftKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        View view = activity.getCurrentFocus();
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
